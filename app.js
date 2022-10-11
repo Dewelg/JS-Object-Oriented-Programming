3c54c6dd20420b3129b3b43062f854478eb03921
_Dewelg_ <_(dewel713@gmail.com_)>
2022-10-11 16:29:07 -0400

    working on it

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Part 1
 class Person{
     constructor(name, pets, residence, hobbies){
         this.name = name;
@@ -16,16 +17,15 @@ class Person{
     }
 
     greeting(){
-        console.log("Hello fellow person!")
+        console.log(`Hello fellow person`);
     }
 }
 
-
+//part2
 class Coder extends Person{
-    constructor(name, occupation){
-        super(name);
-        let occupation = "Full Stack Web Developer";
-        this.occupation = occupation;
+    constructor(){
+        super();
+        this.occupation = "Full Stack Web Developer";
         
     }
 
@@ -33,4 +33,92 @@ class Coder extends Person{
         console.log(`Hello my name is ${this.name} I am a ${this.occupation}`);
     }
 }
+
+//part3
+let man = new Coder("Dewel");
+let work = new Coder("Web Developer");
+
+console.log(man.greeting());
+console.log(work.greeting());
+
+
+//part4
+//idea1
+class Calculator{
+    constructor(result){
+        result = 0;
+    }
+
+    add(a, b){
+        if (a + b){
+            result = a + b;
+            return result;
+        }
+
+    }
+    subtract(a, b){
+        if (a - b){
+            result = a - b;
+            return result;
+        }
+    }
+    divided(a, b){
+        if (a % b){
+            result = a % b;
+            return result;
+        }
+    }
+    multiply(a, b){
+        if (a * b){
+            result = a * b;
+            return result;
+        }
+    }
+}
+
+
+
+//idea2
+class Calculator{
+    constructor(result, number1, number2){
+        this.result = result;
+        this.number1 = number1;
+        this.number2 = number2;
+    }
+
+    add(number1, number2){
+        if (number1 + number2){
+            result = number1 + number2;
+            return result;
+        }
+
+    }
+    subtract(number1, number2){
+        if (number1 - number2){
+            result = number1 - number2;
+            return result;
+        }
+    }
+    divided(number1, number2){
+        if (number1 % number2){
+            result = number1 % number2;
+            return result;
+        }
+    }
+    multiply(number1, number2){
+        if (number1 * number2){
+            result = number1 * number2;
+            return result;
+        }
+    }
+
+    displayResult(){
+        console.log(this.add);
+        console.log(this.subtract);
+        console.log(this.divided);
+        console.log(this.multiply);
+    }
+}
+
+
 
